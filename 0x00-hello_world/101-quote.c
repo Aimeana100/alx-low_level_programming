1967cae9e4e67d0f48a3e2597dea6a4284b6ec87
Aimeana100 <aimeanathole@gmail.com>
2022-03-10 11:11:40 -0800

    program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
+ * main - prints exactly "and that piece of art is useful"
+ * -Dora a Korpar, 2015-10-19",
+ *  followed by a new line. to the standard error.
+ *  Return: Always 0 (Sucess)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
