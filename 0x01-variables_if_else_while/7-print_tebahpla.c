6efb1cd76ce3ed82b1628f0da9e1b44a06699249
Aimeana100 <aimeanathole@gmail.com>
2022-03-14 06:03:00 -0700

    correct 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: a program that prints the 
+ * Description: a program that prints the
  * lowercase alphabet in reverse, followed by a new line.
  *Return: 0
  */
