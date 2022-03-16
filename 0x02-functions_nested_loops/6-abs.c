3b6d8bf73e6a1d06df887a1c820d0c73454a6d37
Aimeana100 <aimeanathole@gmail.com>
2022-03-16 16:33:02 -0700

    get an absulte value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * _abs - main entry blocks
+ * description : compute an absolute value for an intiger
+ * @n : an interger to be take as an input
+ * Return: absolute of n
+ */
+int _abs(int n)
+{
+	return (n<0 ? n*-1 : n)
+}
