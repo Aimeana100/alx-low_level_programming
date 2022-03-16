ab361575c32b3c4e911f8718d71f321f05b88e11
Aimeana100 <aimeanathole@gmail.com>
2022-03-16 12:50:33 -0700

    print a sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,19 +2,19 @@
 /**
  * print_sign - main entry function
  * description : checks 
- * @c : the ansci code character to be checked
+ * @n : the ansci code character to be checked
  * Return: -1,0,1
  */
-int print_sign(int c)
+int print_sign(int n)
 {
-	if(c < 0)
+	if(n < 0)
 	{
 		_putchar('-');
 		_putchar(',');
 		_putchar(' ');
 		return (-1);
 	}
-	else if(c > 0)
+	else if(n > 0)
 	{
 		_putchar('+');
 		_putchar(',');
