05b39ebacc71798b16e57358de270a76f8e4817b
Aimeana100 <aimeanathole@gmail.com>
2022-03-16 12:42:14 -0700

    print a sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,16 +10,22 @@ int print_sign(int c)
 	if(c < 0)
 	{
 		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
 		return (-1);
 	}
 	else if(c > 0)
 	{
 		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
 		return (1);
 	}
 	else
 	{
 		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 		return (1);
 	}
 }
