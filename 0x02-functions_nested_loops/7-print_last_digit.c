ccc63f19a8cf1d18bc565fe3f213b16ffc5ab93c
Aimeana100 <aimeanathole@gmail.com>
2022-03-16 17:50:10 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
  * print_last_digit - main entry function
  * description : print the last digit of a number
  * @n : the ansci code character to be checked
- * Return: -1,1,0
+ * Return: loop
  */
 int print_last_digit(int n)
 {
