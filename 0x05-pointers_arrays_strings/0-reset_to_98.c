af66e1c2024f81ac9f1af0cfe83584a200c92917
Aimeana100 <aimeanathole@gmail.com>
2022-03-21 11:43:09 -0700

    0. 98 Battery st. fitst commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - a  function block
- * description : function that takes a pointer 
+ * description : function that takes a pointer
  * to an int as paramenter and updates the value
  *  it points to 98
  *  return: void
