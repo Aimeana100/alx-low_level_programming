282fe360d56f729403daa2341c6985fbe62298b6
Aimeana100 <aimeanathole@gmail.com>
2022-03-21 12:41:30 -0700

    get the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - function block
+ * descrption: the function that return the length of a string
+ * @s: a pointer to a string or array of characters
+ * return: interger number
+ */
+
+int _strlen(char *s)
+{
+	return strlen(*s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,3 +6,6 @@ void reset_to_98(int *n);
 
 /* swap the values of 2 integers */
 void swap_int(int *a, int *b);
+
+/* return the length of string*/
+int _strlen(char *s);
