3689fe3ca1b0aee2d3d93ccf253238e57d64db22
Aimeana100 <aimeanathole@gmail.com>
2022-03-28 08:00:34 -0700

    create 3-strspn.c to get a length of prefix substring

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,3 +6,17 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 
 /* locate a character in string */
 char *_strchr(char *s, char c);
+/* gets the length of a prefix substring */
+unsigned int _strspn(char *s, char *accept);
+/* searches a string for any of a set of bytes. */
+char *_strpbrk(char *s, char *accept);
+
+/* function that locates a substring. */
+char *_strstr(char *haystack, char *needle);
+
+/* function that prints the chessboard. */
+void print_chessboard(char (*a)[8]);
+
+/* sum of the two diagonals of a square matrix of integers */
+void print_diagsums(int *a, int size);
+
