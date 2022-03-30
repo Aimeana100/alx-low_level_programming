e74b44139b88c584fcae198efaef0190ae8890ca
Aimeana100 <aimeanathole@gmail.com>
2022-03-30 14:17:32 -0700

    recursion 1

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,3 +2,24 @@
 void _putchar(char c);
 /* 0 task */
 void _puts_recursion(char *s);
+
+/* 1 task */
+void _print_rev_recursion(char *s);
+
+/* 2 task */
+int _strlen_recursion(char *s);
+
+/* 3 task */
+int factorial(int n);
+
+/* 4 task */
+int _pow_recursion(int x, int y);
+
+/*5 task */
+int _sqrt_recursion(int n);
+
+/* task 6 */
+int is_prime_number(int n);
+
+/* task 7  */
+
