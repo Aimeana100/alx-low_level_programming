ebee5e7e6432ff567515701a05e4fa9df3476fb6
Aimeana100 <aimeanathole@gmail.com>
2022-04-01 14:26:05 -0700

    argc argv print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,25 +1,17 @@
-#include"main.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - intery block
- * description: print the program name
- * @argc: main function arguments
- * @argv: main function array.
- * return: 0, failure
+ * main - prints its name, followed by a new line
+ * @argc: number of commandline arguments
+ * @argv: list of commandline arguments
+ *
+ * Return: 0 Always(success)
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	if (argc > 0)
-	{
-		while (*argv[0] != '\0')
-		{
-			_putchar(*argv[0]);
-			argv[0]++;
-		}
-	}
-	_putchar('\n');
+	printf("%s\n", argv[0]);
+
+	(void)argc;
 
 	return (0);
 }
