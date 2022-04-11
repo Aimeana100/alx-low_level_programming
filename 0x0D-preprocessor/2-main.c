a2e94554b5f010a8a798d1d63003f3310091ee58
Aimeana100 <aimeanathole@gmail.com>
2022-04-11 16:27:11 -0700

    collect lastly

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - main block print a filename
- * return: always 0 at success
+ * main - prints the name of file
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("%s\n", __FILE__);
