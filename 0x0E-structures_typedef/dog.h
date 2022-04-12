b25db3e35f46bcfa4160bfda7a7117bc00842d4a
Aimeana100 <aimeanathole@gmail.com>
2022-04-11 17:45:25 -0700

    Task 1 0. Poppy create a struct of dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * stuct dog - the stuct dog that should be compired
+ * @name: name parameter
+ * @age: age parameter
+ * @owner: dog owner parameter
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
